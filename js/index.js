9631a1729442532be4818bbee62a9f0d43b79db7
TToiv <toit1_15@uni.worc.ac.uk>
2018-12-04 15:03:52 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -26,7 +26,11 @@ $(document).on('pageinit', function() {
 function createMessage(){		
 	//phoneGap and jQueryMobile do not support toast messages directly
     //so we can add this using toast.js
-    new Toast({content: 'An example message.', duration: 10000}); 	
+    new Toast({content: 'An example message.', duration: 2000}); 	
+}
+
+function createMessage(){
+    new Toast({content: 'another message.', duration: 3000});
 }
         	
 
