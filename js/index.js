3623ed609ea25157260b614e8160224b278c3bf7
TToiv <toit1_15@uni.worc.ac.uk>
2018-12-18 16:31:51 +0000

    fdwe

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 var notification_count=0;
 
-alert("qwerty");
-
 $(document).on('pageinit', function() {
 
 	$('#messageButton').on('click', function() {
@@ -22,30 +20,22 @@ $(document).on('pageinit', function() {
 
 
 
-//
-//section 1
-//
-function createMessage(){
-    
-    alert("create message");
+function createMessage(){		
 	//phoneGap and jQueryMobile do not support toast messages directly
     //so we can add this using toast.js
-    new Toast({content: 'An example message.', duration: 5000}) 	
-}  	
+    new Toast({content: 'An example message.', duration: 1000}); 	
+}
+        	
 
-//
-//section 2
-//
 function createDialog() {
 
-      alert("createDialog ");
 	//phonegap supports native dialog boxes.
 	//here's a simple example
       
 	navigator.notification.confirm(
-    	'Are you hungry',  // message
+    	'Are you hungry?',  // message
         dialogDismissed,         // callback
-        'Check up',            // title
+        'Breaktime request',            // title
         ['Yes', 'No']                  // buttons
     );
 
@@ -53,43 +43,24 @@ function createDialog() {
         	
         	
         	
-function dialogDismissed(buttonIndex) 
-{
-	alert("dialogDismissed ");
-	    
-    if(buttonIndex==1) 
-        {
-        new Toast({content: "Take a break and eat something.", duration: 3000});
-   
-            cordova.plugins.notification.local.schedule
-                ({
-                    id:     1,
-                    title: "Break over?",
-                    text: "Back to work, peon!",
-                    trigger: { in 30, unit: 'second' }
-                });
-            
-        }
-   	else if(buttonIndex==2) new Toast({content: 'Carry on working.', duration: 3000});
+function dialogDismissed(buttonIndex) {
+	
+	if(buttonIndex==1) new Toast({content: "Take a break", duration: 3000});
+   	else if(buttonIndex==2) new Toast({content: 'Carry on then.', duration: 3000});
 
 }
 
    
    
-//
-//section 3
-//
-function createNotification() 
-{
-
-      alert("createNotification ");  		
+function createNotification() {
+        		
     			
     //
     //setup notification
     //
 	cordova.plugins.notification.local.schedule({ 
-    	id: 	1,
-        title: 	"Hey you",
+    	id: 		1,
+        title: 		"Hey you",
         text: 	"This is an example notification",
         trigger: { in: 10, unit: 'second' }
    	});
